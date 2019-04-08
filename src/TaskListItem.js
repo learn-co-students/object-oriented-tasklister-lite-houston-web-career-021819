a66ec754ec5006d758d4121f02dd36828919a3ef
github username <github email address>
2019-04-08 15:24:54 -0500

    final changes

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,55 @@
 class TaskListItem {
-  
+
+    constructor(createTaskForm) {
+        const inputField = createTaskForm.querySelector('#new-task-description')
+        this.description = inputField.value + " "
+        const selectedColor = createTaskForm.querySelector('#text-color')
+        this.color = selectedColor.value
+        const dueDate = createTaskForm.querySelector('#due-date')
+        this.dueDate = dueDate.value
+        this.deleted = false
+
+        TaskListItem.all.push(this)
+    }
+
+    static notDeleted(){
+        let notDeletedItems = []
+        TaskListItem.all.forEach(function(item){
+            if (item.deleted === false) {
+                notDeletedItems.push(item)
+            }
+        })
+        return notDeletedItems
+    }
+    static HighPriority(){
+        let highPriorityItems = []
+        TaskListItem.notDeleted().forEach(function(item){
+            if (item.color === "high") {
+                highPriorityItems.push(item)
+            }
+        })
+        return highPriorityItems
+    }
+
+    static MediumPriority(){
+        let mediumPriorityItems = []
+        TaskListItem.notDeleted().forEach(function(item){
+            if (item.color === "medium") {
+                mediumPriorityItems.push(item)
+            }
+        })
+        return mediumPriorityItems
+    }
+
+    static LowPriority(){
+        let lowPriorityItems = []
+        TaskListItem.notDeleted().forEach(function(item){
+            if (item.color === "low") {
+                lowPriorityItems.push(item)
+            }
+        })
+        return lowPriorityItems
+    }
 }
+
+TaskListItem.all = []
