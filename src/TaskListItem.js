35fac3339b1a20dc70050eeb2f435216a051ac63
Mark R Pothecary <markpothers@hotmail.com>
2019-04-08 16:02:11 -0500

    mostly complete

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,51 @@
 class TaskListItem {
-  
+
+    constructor(description, duedate, priority='high'){
+        this.element = document.createElement('li')
+        this.priority = priority
+        this.duedate = duedate
+        this.element.innerText = `${description}, ${duedate}`
+        allTasks.push(this)
+    }
+
+    color(){
+        if (this.priority === 'high'){
+            this.element.style.color = 'red'
+        } else if  (this.priority === 'medium'){
+            this.element.style.color = 'yellow'
+        } else if (this.priority === 'low'){
+            this.element.style.color = 'green'
+        }
+    }
+
+    delete(){
+        this.element.remove()
+        let index = allTasks.indexOf(this)
+        allTasks.splice(index, 1)
+    }
+
+    static sort(){
+        let sorted = []
+        allTasks.forEach(function(item){
+            if (item.priority === 'low'){
+                sorted.push(item)
+            }
+        })
+        allTasks.forEach(function(item){
+            if (item.priority === 'medium'){
+                sorted.push(item)
+            }
+        })
+        allTasks.forEach(function(item){
+            if (item.priority === 'high'){
+                sorted.push(item)
+            }
+        })
+        sorted.forEach(function(item){
+            TaskList.addListItem()
+        }
+ 
+    }
+
 }
+const allTasks = []
--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const taskList = new TaskList();
 
   const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+  const renderApp = () => (listContainer.append(taskList.render()));
 
   // Add Form Behavior Here!
+  const form = document.querySelector('#create-task-form')
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let description = document.querySelector('#new-task-description')
+    let duedate = document.querySelector('#new-task-duedate')
+    taskList.newItem(description, duedate.value)
+    description.value = ""
+      })
 
   renderApp()
 });
