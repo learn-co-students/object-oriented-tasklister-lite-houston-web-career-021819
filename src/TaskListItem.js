8f7897f5e8930cd1e1c234cf5df3295c578ca87e
EliLauff <eli.lauffenburger@gmail.com>
2019-04-08 15:49:59 -0500

    done

--- src/TaskListItem.js
+++ src/TaskListItem.js
@@ -1,3 +1,12 @@
 class TaskListItem {
-  
+  constructor(description) {
+    this.description = description;
+
+    if (TaskListItem.all == null) {
+      TaskListItem.all = [];
+      TaskListItem.all.push(this);
+    } else {
+      TaskListItem.all.push(this);
+    }
+  }
 }
