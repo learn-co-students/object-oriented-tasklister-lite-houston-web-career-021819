055c232fe321345c8c28c1b1a9b4f6068ff9211b
github username <github email address>
2019-04-09 09:58:36 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const taskList = new TaskList();
+  const form = document.querySelector("#create-task-form")
+  const taskDescriptionInput = document.querySelector('#new-task-description')
+  const listContainer = document.querySelector("#list");
 
-  const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
+  const taskList = new TaskList()
+
+  listContainer.append(taskList.element)
 
   // Add Form Behavior Here!
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const description = taskDescriptionInput.value
+    taskList.createItem(description)
+    taskDescriptionInput.value = ''
+  })
 
-  renderApp()
 });
